9e759c9d4942503031ec8c93a0b401b4cbfb204c
chrislavin <chrislavin@f9c30f94-93db-42ac-86b4-79fac9d30f2c>
2011-04-13 21:00:43 +0000

    Fixed a bug in the removeInstance() method.

--- trunk/edu/byu/ece/rapidSmith/design/Design.java
+++ trunk/edu/byu/ece/rapidSmith/design/Design.java
@@ -500,9 +500,10 @@ public class Design implements Serializable{
 			// TODO - We can sort through PIPs to only remove those that need
 			// to be removed, we just need a method to do that
 			p.getNet().unroute(); 
-			p.getNet().removePin(p);
 			if(p.getNet().getPins().size() == 1){
-				nets.remove(p.getName());
+				nets.remove(p.getNet().getName());
+			}else{
+				p.getNet().removePin(p);				
 			}
 		}
 		instances.remove(instance.getName());
